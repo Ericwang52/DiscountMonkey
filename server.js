be1f661860445877df63be573db34fca7762b09f
Eric Wang <ericwang52@yahoo.ca>
2021-01-09 22:22:02 -0500

    changed port

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ if(process.env.NODE_ENV === "production") {
     })
   }
   
-  const port = 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+  const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
   app.listen(port, () => console.log(`Server up and running on port ${port} !`));
