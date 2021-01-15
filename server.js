c66d1bb49509fdaa1f348610d1a07aaf61002cdf
Eric Wang <ericwang52@yahoo.ca>
2021-01-14 23:53:59 -0500

    fixed heroku

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ app.use(express.json());
 app.use("/api", api)
 app.use(express.static('client/build'))
     console.log(process.env.NODE_ENV === "production")
-// if(process.env.NODE_ENV === "production") {
-//     //Set static folder
-//     app.use(express.static('client/build'))
-//     app.get('*', (req,res)=>{
-//       res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-//     })
-//   }
+if(process.env.NODE_ENV === "production") {
+    //Set static folder
+    app.use(express.static('client/build'))
+    app.get('*', (req,res)=>{
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+  }
   
   const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
   app.listen(port, () => console.log(`Server up and running on port ${port} !`));
